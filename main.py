1db7e6c4b9440c643d208981b07fcec6416224b9
Shaik Azhar Sohail <155532610+SohailShaikAzhar@users.noreply.github.com>
2024-07-02 21:51:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import json
 #difflib helps to find the differences between strings or any data type which is hashable(dict)
-#get_close_matches helps to get the best matches which we give as input
+#get_close_matches helps to get the best matches which we give as input based on the percentage of matching of strings
 from difflib import get_close_matches
 
-#load the knowledge base from json file
+#load the knowledge base from json file 
 def load_knowledge_base(file_path: str)-> dict:
     with open(file_path, 'r') as file:
         data: dict = json.load(file)
     return data
 
+#opening 
 def save_knowledge_base(file_path: str, data: dict):
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=2)
 
+#finding the best match based on the input given 
 def find_best_match(user_question: str, questions: list[str]) -> str | None:
     matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)
     return matches[0] if matches else None
 
+#Finding the answer to the question
 def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
     for q in knowledge_base["questions"]:
         if q["question"] == question:
@@ -24,13 +27,16 @@ def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
     
 
 def chat_bot():
+    #assigning the knowledge_base as dict from json file
     knowledge_base: dict = load_knowledge_base('knowledge_base.json')
 
+    #taking the input from the user
     while True:
         user_input: str = input('You: ')
         if user_input.lower() == 'quit':
             break
 
+        #finding the matching string with the user input for the output
         best_match: str | None = find_best_match(user_input, [q["question"] for q in knowledge_base["questions"]])
         if best_match:
             answer: str = get_answer_for_question(best_match, knowledge_base)
